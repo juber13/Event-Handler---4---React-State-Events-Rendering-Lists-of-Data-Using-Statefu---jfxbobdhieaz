5c5a7adc6800cc486e13ba20d21d2be0c771bf1c
juber <juberkhan0707@gmail.com>
2023-01-26 10:41:00 +0530

    make double click method

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React,{useState} from 'react'
 import '../styles/App.css';
 const App = () => {
+ const [isTrue , setTrue] = useState(false);
 
-  const handleDoubleClick = (event) =>{
-    if(event.isTrusted) console.log("I was double clicked" );
-    else console.log("I was not double clicked");
-  }
+ 
+ const handleDoubleClick = (event) =>{
+   if(event.detail === 2) console.log("I was double clicked" );
+   else if(event.detail === 1)
+    console.log("I was not double cliked");
+}
+
   return (
     <div id="main">
-      <button id="dblclick-btn" onDoubleClick={handleDoubleClick}>Double click me</button>
+      <button id="dblclick-btn" onClick={handleDoubleClick}>Double click me</button>
     </div>
   )
 }
