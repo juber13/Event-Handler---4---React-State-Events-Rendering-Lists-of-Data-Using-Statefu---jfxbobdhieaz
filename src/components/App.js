2b5eea9bc9a1c03e3deea6e5a1b58a9e68f619f4
juber <juberkhan0707@gmail.com>
2023-01-26 10:03:16 +0530

    make double

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,12 @@ import '../styles/App.css';
 const App = () => {
 
   const handleDoubleClick = (event) =>{
-  
+    if(event.isTrusted) console.log("I was double clicked" );
+    else console.log("I was not double clicked");
   }
   return (
     <div id="main">
-      <button id="dblclick-btn">Double click me</button>
+      <button id="dblclick-btn" onDoubleClick={handleDoubleClick}>Double click me</button>
     </div>
   )
 }
